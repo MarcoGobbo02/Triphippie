1c74d34c96e070a957a401e3cf661fe3c52533d0
MarcoGobboMTSS <marco.gobbo2002@gmail.com>
2024-07-15 11:58:33 +0200

    creazione progetto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimeNGConfig } from 'primeng/api';
+import { AuthService } from './servizi/auth.service';
+import { CommonModule } from '@angular/common';
+import { MenuHeaderComponent } from './componenti/utilities/menu-header/menu-header.component';
+
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, CommonModule, MenuHeaderComponent]
 })
 export class AppComponent {
-  title = 'triphippie';
+  
+  constructor(private primengConfig: PrimeNGConfig, private authservice: AuthService) {}
+
+
+  
+  
 }
